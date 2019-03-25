c9aafdced16707e14267275c6df9f9ad9debc89e
Anson5555555Lee <frank5555555li@gmail.com>
2019-03-25 14:12:36 +0800

    init commit for dev branch

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const App = () => <h1>Hello WandWallet</h1>
+const App = () => <h1>Hello Anson!!!</h1>
 
 export default App
--- src/main.dev.js
+++ src/main.dev.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron')
+import { app, BrowserWindow } from 'electron'
 
 let mainWindow
 
