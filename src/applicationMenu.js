94b595c1439102348df16251a01a097199392e20
Anson5555555Lee <frank5555555li@gmail.com>
2019-03-28 16:25:06 +0800

    add submenu

--- src/applicationMenu.js
+++ src/applicationMenu.js
@@ -1,8 +1,9 @@
 import { app, BrowserWindow, Menu, shell } from 'electron'
 import { APP_NAME } from '../config/common'
 
-console.log(`APP NAME: ${APP_NAME}`)
-console.log(app.getName())
+// const createApplicationMenu = () => {
+
+// }
 
 const template = [
     {
@@ -19,11 +20,33 @@ const template = [
                 role: 'paste'
             }
         ]
+    },
+    {
+        label: 'Help',
+        role: 'help',
+        submenu: [
+            {
+                label: 'Visit Website',
+                click() {}
+            },
+            {
+                label: 'Toggle Developer Tools',
+                click() {}
+            }
+        ]
     }
 ]
 
 if (process.platform === 'darwin') {
-    template.unshift({label: APP_NAME})
+    template.unshift({
+        label: `${APP_NAME}`,
+        submenu: [
+            {
+                label: `About ${APP_NAME}`,
+                role: 'about'
+            }
+        ]
+    })
 }
 
 export default Menu.buildFromTemplate(template)
