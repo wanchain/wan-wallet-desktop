788d563b3809a138c638e26c3a4d2687622600d1
Anson5555555Lee <frank5555555li@gmail.com>
2019-04-03 14:33:07 +0800

    modify ui

--- src/app/containers/portfolio/portfolio.js
+++ src/app/containers/portfolio/portfolio.js
@@ -1,18 +1,85 @@
 import React, { Component } from 'react';
+
 import { Layout } from 'antd';
+
+import path from 'path'
+
+
 
 const { Content } = Layout;
 
+
+
+const { remote, ipcRenderer } = window.require('electron');
+
+const mainProcess = remote.require(path.join(__dirname, '../controllers/index.js'));
+
+const currentWindow = remote.getCurrentWindow();
+
+
+
 class Portfolio extends Component {
-  render () {
-    return (
-      <Layout>
-        <Content style={{padding: 24, background: '#13175C', minHeight:"90vh", color:'#fff'}}>
-          Portfolio
-        </Content>
-      </Layout>
-    );
-  }
+
+
+
+  constructor(props) {
+
+    super(props)
+
+    this.state = {
+
+        phrase: 'I am just a placeholder :-)'
+
+    }
+
 }
+
+
+
+handleClick = () => {
+
+    console.log(mainProcess.generatePhrase)
+
+    mainProcess.generatePhrase(currentWindow, '123')
+
+}
+
+
+
+componentDidMount() {
+
+    ipcRenderer.on('phrase-generated', (event, phrase) => {
+
+        this.setState({phrase: phrase})
+
+    })
+
+}
+
+
+
+render() {
+
+    return (
+
+        <div>
+
+            <h1>{this.state.phrase}</h1>
+
+            <button onClick={this.handleClick}>
+
+            Give me a phrase!!!
+
+            </button>
+
+        </div>
+
+    );
+
+}
+
+}
+
+
 
 export default Portfolio;
