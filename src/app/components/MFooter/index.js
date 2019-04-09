714b3e8a8517848594e43a7dfca33a2bfe57b8d0
jasonHJ <hejian_best@163.com>
2019-04-09 18:59:29 +0800

    add Footer component

---
+++ src/app/components/MFooter/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.less';
+
+class MFooter extends Component {
+    render() {
+        return (
+            <div className="footer">
+              All rights reserved <br/> ©2019 WANCHAIN FOUNDATION LTD
+            </div>
+        );
+    }
+}
+
+export default MFooter;
