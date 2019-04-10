f237ff51337d46a5f55c45703a505abf0ec40068
jasonHJ <hejian_best@163.com>
2019-04-10 14:51:45 +0800

    add util func

---
+++ src/app/utils/support.js
@@ -0,0 +1,15 @@
+export function accumulator(promise, func) {
+  return promise.then(result => {
+    return func().then(Array.prototype.concat.bind(result));
+  });
+};
+
+
+export function getBalanceObj(arr) {
+  let newArr = {};
+  arr.forEach((item) => {
+    let key = Object.keys(item);
+    newArr[key] = item[key];
+  });
+  return newArr;
+}
