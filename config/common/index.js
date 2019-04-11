7f1c759bb27ee0ae9e69154407e02f2e897ad8e7
Anson5555555Lee <frank5555555li@gmail.com>
2019-04-11 12:32:13 +0800

    modify

--- config/common/index.js
+++ config/common/index.js
@@ -1,6 +1,7 @@
 import CHAN from './channels'
 
 export const WDS_PORT = 7000
+export const isDev = process.env.NODE_ENV === 'development' 
 export const STATIC_PATH = '/static'
 
 export const APP_ROOT = 'root'
