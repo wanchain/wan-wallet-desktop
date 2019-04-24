0534f1be9a1d6ff09983a9620c7f5f5ac7ed11f0
Anson5555555Lee <frank5555555li@gmail.com>
2019-04-24 17:56:06 +0800

    fix

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 import backend from '~/src/modules/walletBackend'
 // import manager from '~/src/modules/wanchainNode/clientBinaryManager'
 import windows from '~/src/modules/windows'
+import _updater from '~/src/modules/updater'
 
 export const walletBackend = backend
 
@@ -8,4 +9,6 @@ export const walletBackend = backend
 
 export const Windows = windows
 
-export default { walletBackend, Windows }
+export const updater = _updater
+
+export default { walletBackend, Windows, updater }
