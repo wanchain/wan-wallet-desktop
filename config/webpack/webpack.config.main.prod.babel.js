44aa6ae715f794e3367716d6d4dea778cca564fb
Anson5555555Lee <frank5555555li@gmail.com>
2019-04-28 18:03:40 +0800

    add webpack config for main and preload transpile

---
+++ config/webpack/webpack.config.main.prod.babel.js
@@ -0,0 +1,52 @@
+import path from 'path'
+import nodeExternals from 'webpack-node-externals'
+
+export default {
+    externals: [nodeExternals()],
+
+    target: 'electron-main',
+
+    devtool: 'source-map',
+    
+    mode: "production",
+    
+    entry: {
+        main: './src/main.dev.js',
+        preload: './src/modules/preload/index.js'
+    },
+    
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, '..', '..', 'build'),
+    },
+    
+    module: {
+        unknownContextCritical : false,
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true
+                    }
+                }
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    
+    node: {
+        __dirname: false,
+        __filename: false
+    }
+}
+
