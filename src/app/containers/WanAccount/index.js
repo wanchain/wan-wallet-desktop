1bd8bebe92c6a266c77b34f0b7547b2068145255
jasonHJ <hejian_best@163.com>
2019-05-05 11:13:02 +0800

    update

--- src/app/containers/WanAccount/index.js
+++ src/app/containers/WanAccount/index.js
@@ -46,11 +46,7 @@ class WanAccount extends Component {
     {
       title: 'ACTION',
       dataIndex: 'action',
-      render: (text, record) => {
-        console.log(text, 'ppppppppllllll')
-        console.log(record, 'ppppppppllllll')
-        return <div><SendNormalTrans from={text} handleSend={this.handleSend}/></div>
-      }
+      render: (text, record) => <div><SendNormalTrans from={record.address} handleSend={this.handleSend}/></div>
     }
   ];
 
