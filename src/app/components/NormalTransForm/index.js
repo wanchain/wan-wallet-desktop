263e230e3277b11ad7d120d5b9efece9d384d748
jasonHJ <hejian_best@163.com>
2019-06-04 17:09:41 +0800

    change icon

--- src/app/components/NormalTransForm/index.js
+++ src/app/components/NormalTransForm/index.js
@@ -247,7 +247,7 @@ class NormalTransForm extends Component {
               </Form.Item>
               <Form.Item label={intl.get('NormalTransForm.amount')}>
                 {getFieldDecorator('amount', { rules: [{ required: true, message: intl.get('NormalTransForm.amountIsIncorrect'), validator: this.checkAmount }] })
-                  (<Input disabled={disabledAmount} min={0} placeholder='0' prefix={<Icon type="money-collect" className="colorInput" />} />)}
+                  (<Input disabled={disabledAmount} min={0} placeholder='0' prefix={<Icon type="dollar" className="colorInput" />} />)}
                 <Checkbox onChange={this.sendAllAmount}>{intl.get('NormalTransForm.sendAll')}</Checkbox>
               </Form.Item>
               {settings.reinput_pwd 
