6fb2c56158cfb06732f2a238066b65dbcfa5d499
Anson5555555Lee <frank5555555li@gmail.com>
2019-06-05 12:27:52 +0800

    cleanup

--- src/modules/windows/index.js
+++ src/modules/windows/index.js
@@ -29,7 +29,6 @@ class Window extends EventEmitter {
             height: 720,
             acceptFirstMouse: true,
             darkTheme: true,
-            // titleBarStyle: 'hiddenInset',
             webPreferences: {
                 webaudio: true,
                 webgl: false,
@@ -45,7 +44,6 @@ class Window extends EventEmitter {
         this.window = new BrowserWindow(electronOptions)
 
         this.session = this.window.webContents.session
-        // this.session.setUserAgent(this.session.getUserAgent(), setting.language)
 
         this.webContents = this.window.webContents
 
@@ -296,8 +294,6 @@ class Windows {
     }
 
     getById(id) {
-        // logger.debug(`Get window by id, ${id}`)
-
         return _.find(this._windows, (w) => w.id === id )
     }
 
