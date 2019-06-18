b4166280eb4ea0b5d242c69d15eec049c619e344
Anson5555555Lee <frank5555555li@gmail.com>
2019-06-18 17:39:37 +0800

    add build-mac.sh

---
+++ build-mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf build/
+rm -rf dist/ 
+
+mkdir build
+
+cp -rf assets build/
+cp -rf src/modals build/
+cp -rf config/i18n/locales build/
+cp src/index.html build/
+cp entitlements.mac.plist build/
