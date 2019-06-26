9cceabf02b76724af1c9b0c3dd8c0976a39f4573
Anson5555555Lee <frank5555555li@gmail.com>
2019-06-26 09:02:53 +0800

    update notarize script

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -7,13 +7,23 @@ exports.default = async function notarizing(context) {
     const appName = context.packager.appInfo.productFilename;
 
     if (electronPlatformName !== 'darwin') {
-        return;
+        return
     }
 
-    return await notarize({
-        appBundleId: 'org.wallet.wanchain',
-        appPath: `${appOutDir}/${appName}.app`,
-        appleId: process.env.APPLEID,
-        appleIdPassword: process.env.APPLEIDPASS,
-    })
+    console.log('electronPlatformName: ', electronPlatformName)
+    console.log('appOutDir: ', appOutDir)
+    console.log('appName: ', appName)
+    console.log('appleId: ', process.env.APPLEID)
+    console.log('appleIdPassword: ', process.env.APPLEIDPASS)
+
+    try {
+        return await notarize({
+            appBundleId: 'org.wallet.wanchain',
+            appPath: `${appOutDir}/${appName}.app`,
+            appleId: process.env.APPLEID,
+            appleIdPassword: process.env.APPLEIDPASS,
+        })
+    } catch (err) {
+        console.error('notarizing error: ', err)
+    }    
 }
