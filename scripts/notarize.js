3550b844fe39e65218824158d813cc3f38d22300
Anson5555555Lee <frank5555555li@gmail.com>
2019-06-28 15:08:08 +0800

    add notary script

---
+++ scripts/notarize.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize')
+
+exports.default = async function notarizing(context) {
+    const { electronPlatformName, appOutDir } = context;
+    const appName = context.packager.appInfo.productFilename;
+
+    if (electronPlatformName !== 'darwin') {
+        return
+    }
+
+    console.log('electronPlatformName: ', electronPlatformName)
+    console.log('appOutDir: ', appOutDir)
+    console.log('appName: ', appName)
+
+    console.log('start notarizing...')
+    try {
+        return await notarize({
+            appBundleId: 'org.wallet.wanchain',
+            appPath: `${appOutDir}/${appName}.app`,
+            appleId: process.env.APPLEID,
+            appleIdPassword: process.env.APPLEIDPASS,
+        })
+    } catch (err) {
+        console.error('notarizing error: ', err)
+    }    
+}
