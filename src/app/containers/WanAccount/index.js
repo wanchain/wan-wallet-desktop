e7575a00db40bffd37a4463299b79384917fab9d
ZaneCui <897140741@qq.com>
2019-07-31 16:25:34 +0800

    See private info in wallet.

--- src/app/containers/WanAccount/index.js
+++ src/app/containers/WanAccount/index.js
@@ -2,7 +2,7 @@ import wanUtil from "wanchain-util";
 import intl from 'react-intl-universal';
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { Button, Table, Row, Col, message } from 'antd';
+import { Button, Table, Row, Col, message, Tooltip } from 'antd';
 
 import './index.less';
 import totalImg from 'static/image/wan.png';
@@ -11,6 +11,7 @@ import CopyAndQrcode from 'components/CopyAndQrcode';
 import SendNormalTrans from 'components/SendNormalTrans';
 import { checkAddrType, hasSameName } from 'utils/helper';
 import { EditableFormRow, EditableCell } from 'components/Rename';
+import arrow from 'static/image/arrow.png';
 
 const WALLETID = 1;
 const KEYSTOREID = 5;
@@ -45,19 +46,28 @@ class WanAccount extends Component {
   columns = [
     {
       dataIndex: 'name',
-      editable: true
+      editable: true,
+      width: '15%'
     },
     {
       dataIndex: 'address',
-      render: text => <div className="addrText"><p className="address">{text}</p><CopyAndQrcode addr={text} /></div>
+      render: text => <div className="addrText"><p className="address">{text}</p><CopyAndQrcode addr={text} /></div>,
+      width: '50%'
     },
     {
       dataIndex: 'balance',
       sorter: (a, b) => a.balance - b.balance,
+      width: '20%'
     },
     {
       dataIndex: 'action',
-      render: (text, record) => <div><SendNormalTrans from={record.address} path={record.path} handleSend={this.handleSend} chainType={CHAINTYPE}/></div>
+      render: (text, record) => <div><SendNormalTrans buttonClassName='actionButton' from={record.address} path={record.path} handleSend={this.handleSend} chainType={CHAINTYPE}/></div>,
+      width: '10%'
+    }, 
+    {
+      dataIndex: '',
+      key: 'expand',
+      width: '5%'
     }
   ];
 
@@ -150,6 +160,40 @@ class WanAccount extends Component {
     }
   }
 
+  customExpandIcon = props => {
+    let text;
+    if (props.expanded) {
+      text = 'arrow-down';
+    } else {
+      text = 'arrow-right';
+    }
+    return (
+      <img
+        src={arrow}
+        onClick={e => props.onExpand(props.record, e)}
+        className={text}
+        style={{ width: '12px', height: '10px', cursor: 'pointer' }}
+      />
+    );
+  }
+
+  expandContent = record => {
+    const privateAddress = "0x022b49af7f2a4fd132acee36ce22a81e89f5abc9c824e2cee5e193f8c3b43c314d02aefe897a20968f424dbef269e4aad165a631097548e9fb20c4932beb152e9d32";
+    return (
+      <table style={{width: 'calc(100% + 32px)', position: 'relative', left: '-16px'}}>
+        <tbody>
+          <tr>
+            <td style={{width: '15%', padding: '0px 16px'}}></td>
+            <td style={{width: '50%', padding: '0px 16px'}}><div className="addrText"><p className="privateAddress"><Tooltip placement="bottomLeft" title={privateAddress}>{privateAddress}</Tooltip>{privateAddress}</p><CopyAndQrcode addr={privateAddress}/></div></td>
+            <td style={{width: '20%', padding: '0px 16px'}}>100.00</td>
+            <td style={{width: '10%', padding: '0px 16px'}}><Button className="redeemButton" type="primary">Redeem</Button></td>
+            <td style={{width: '5%', padding: '0px 16px'}}></td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  };
+
   render() {
     const { getAmount, getAddrList } = this.props;
     const components = {
@@ -166,14 +210,15 @@ class WanAccount extends Component {
     return (
       <div className="account">
         <Row className="title">
-          <Col span={12} className="col-left"><img className="totalImg" src={totalImg} alt={intl.get('WanAccount.wanchain')} /> <span className="wanTotal">{getAmount}</span><span className="wanTex">{intl.get('WanAccount.wan')}</span></Col>
+          <Col span={12} className="col-left"><img className="totalImg" src={totalImg} alt={intl.get('WanAccount.wanchain')} /><span className="wanTotal">{getAmount}</span><span className="wanTex">{intl.get('WanAccount.wan')}</span></Col>
           <Col span={12} className="col-right">
-          <Button className="creatBtn" type="primary" shape="round" size="large" onClick={this.creatAccount}>{intl.get('WanAccount.create')}</Button>
+            <Button className="creatBtn" type="primary" shape="round" size="large" onClick={this.creatAccount}>{intl.get('WanAccount.create')}</Button>
           </Col>
         </Row>
         <Row className="mainBody">
           <Col>
-            <Table components={components} rowClassName={() => 'editable-row'} className="content-wrap" pagination={false} columns={this.columnsTree} dataSource={getAddrList} />
+            <Table components={components} rowClassName={() => 'editable-row'} className="content-wrap" pagination={false} columns={this.columnsTree} dataSource={getAddrList}
+              expandedRowRender={this.expandContent} expandIconAsCell={false} expandIconColumnIndex={4} expandIcon={this.customExpandIcon} />
           </Col>
         </Row>
         <Row className="mainBody">
