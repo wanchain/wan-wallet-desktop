2e2a98ac31d64b3cd4de99eac54dadb13519fa66
Anson5555555Lee <frank5555555li@gmail.com>
2019-07-31 13:51:49 +0800

    fix font loading unfound issue

--- build-mac.sh
+++ build-mac.sh
@@ -9,4 +9,9 @@ cp -rf icons build/
 cp -rf src/modals build/
 cp -rf config/i18n/locales build/
 cp src/index.html build/
+
+mkdir -p build/static/font
+cp static/font/Roboto-Bold.ttf build/static/font/
+cp static/font/Roboto-Regular.ttf build/static/font/
+
 cp entitlements.mac.plist build/
