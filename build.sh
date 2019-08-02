2af208e4ff21c92bd1d3f931f54a7ed019c1198f
Anson5555555Lee <frank5555555li@gmail.com>
2019-08-02 15:18:01 +0800

    modify shell scripts

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ cp -rf icons build/
 cp -rf src/modals build/
 cp -rf config/i18n/locales build/
 cp src/index.html build/
+
+mkdir -p build/static/font
+cp static/font/Roboto-Bold.ttf build/static/font/
+cp static/font/Roboto-Regular.ttf build/static/font/
