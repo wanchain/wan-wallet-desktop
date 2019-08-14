7084ddd4193a1a3e4d7264146b1afa2b9e0600b7
JasonHJ <hejian_best@163.com>
2019-08-14 16:43:48 +0800

    update

--- src/app/containers/Offline/index.js
+++ src/app/containers/Offline/index.js
@@ -122,10 +122,10 @@ class Offline extends Component {
           <Button type="primary" onClick={this.handleGetInfo} className="getInfoBtn">{intl.get('Offline.getInfo')}</Button>
           <Row align="middle" style={{ marginTop: '10px' }}>
             <Col span={6}>
-              <span className="textFont">Gas Price: </span><Statistic valueStyle={statStyle} value={gasPrice === null ? '--' : parseInt(gasPrice) + ' Gwin'} />
+              <span className="textFont">{intl.get('AdvancedOptionForm.gasPrice')}: </span><Statistic valueStyle={statStyle} value={gasPrice === null ? '--' : parseInt(gasPrice) + ' Gwin'} />
             </Col>
             <Col span={6}>
-              <span className="textFont">Nonce: </span><Statistic valueStyle={statStyle} value={nonce === null ? '--' : nonce} />
+              <span className="textFont">{intl.get('AdvancedOptionForm.nonce')}: </span><Statistic valueStyle={statStyle} value={nonce === null ? '--' : nonce} />
             </Col>
           </Row>
         </div>
