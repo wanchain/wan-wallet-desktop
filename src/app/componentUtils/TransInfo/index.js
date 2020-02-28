1e5d8e403727d4650842a2baca2e5a672d78ba31
ZaneCQ <897140741@qq.com>
2020-02-28 17:45:07 +0800

    Change the url of lockHash in EOS cross-chain transaction info.

--- src/app/componentUtils/TransInfo/index.js
+++ src/app/componentUtils/TransInfo/index.js
@@ -23,7 +23,7 @@ class TransInfo extends Component {
     }
     let href = this.props.chainId === 1 ? `${MAIN}/tx/${hash}` : `${TESTNET}/tx/${hash}`;
     if (chain === 'EOS') {
-      href = `https://jungle.bloks.io/transaction/${hash}`
+      href = this.props.chainId === 1 ? `https://www.wanscan.org/tx/0x${hash}` : `https://testnet.wanscan.org/tx/0x${hash}`;
     }
     wand.shell.openExternal(href);
   }
