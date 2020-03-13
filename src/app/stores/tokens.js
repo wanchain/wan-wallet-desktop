d1f18b8bb328e3ed192e925c14ef3b4d7cceb805
JasonHJ <hejian_best@163.com>
2020-03-13 11:33:41 +0800

    fix amount

--- src/app/stores/tokens.js
+++ src/app/stores/tokens.js
@@ -410,8 +410,8 @@ class Tokens {
     importArr.concat(ledgerArr).concat(trezorArr).forEach(item => {
       let balance;
       if (self.tokensBalance && self.tokensBalance[self.currTokenAddr]) {
-        if (self.formatTokensList && self.formatTokensList[self.currTokenAddr]) {
-          balance = formatNumByDecimals(self.tokensBalance[self.currTokenAddr][item], self.formatTokensList[self.currTokenAddr].decimals)
+        if (self.tokensList && self.tokensList[self.currTokenAddr]) {
+          balance = formatNumByDecimals(self.tokensBalance[self.currTokenAddr][item], self.tokensList[self.currTokenAddr].decimals)
         } else {
           balance = 0
         }
