ab13b2808213532279438689b2a66ecfe55e4a10
Gabriel <pkguowu@gmail.com>
2020-09-10 18:25:13 +0800

    change api server

--- src/modules/walletBackend/config.js
+++ src/modules/walletBackend/config.js
@@ -23,35 +23,36 @@ class ConfigFactoryService {
 
         } else {
             this.config.wanchain_js_testnet = true
-            /* this.config.iWAN = {
+            this.config.iWAN = {
                 "url": ['apitest.wanchain.org'],
                 "port": [8443],
                 "wallet": {
                     "apikey": process.env.API_KEY,
                     "secret": process.env.SECRET_KEY
                 }
-            } */
-            this.config.iWAN = {
-                "url": ['192.168.1.179'],
-                "port": [8443],
-                "wallet": {
-                    "apikey": "d21b98b09c1b4f1001986401e25a27a07a4673140b5125b81cdfedcea4db9e7b",
-                    "secret": "93c30e4a70f5ec3d4427f76602851791aa58fb823773c96cf1347f8b0276b036"
-                }
             }
+            // this.config.iWAN = {
+            //     "url": ['192.168.1.179'],
+            //     "port": [8443],
+            //     "wallet": {
+            //         "apikey": "d21b98b09c1b4f1001986401e25a27a07a4673140b5125b81cdfedcea4db9e7b",
+            //         "secret": "93c30e4a70f5ec3d4427f76602851791aa58fb823773c96cf1347f8b0276b036"
+            //     }
+            // }
         }
 
         this.config.logPathPrex = WalletHelper.getLogPath()
         this.config.databasePathPrex = path.join(dataPathPrex, `${this.config.network}DB`)
         this.config.walletPathPrex = path.join(dataPathPrex, 'walletDB')
         this.config.logtofile = true
+        // this.config.logtofile = false
         this.config.logfile = 'wanWalletSDK'
         this.config.MAX_CONFIRM_BLKS = 100000000
         this.config.MIN_CONFIRM_BLKS = 0
         this.config.dbExtConf = {
             "userTblVersion": DB_VERSION
         }
-        this.config.loglevel = 'debug'
+        // this.config.loglevel = 'debug'
     }
 
     getConfig() {
