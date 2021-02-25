5a4f2d2cbb1b4362c2257771bf4a7c9fa6337791
ZaneCui <897140741@qq.com>
2021-02-25 10:36:28 +0800

    Upload upgrade.json when packaging in Windows OS.

--- scripts/afterBuild.js
+++ scripts/afterBuild.js
@@ -1,7 +1,7 @@
 const os = require('os');
 
 exports.default = function () {
-    if(os.type() === 'Linux') {
+    if(os.type() === 'Windows_NT') {
         return ['upgrade.json'];
     } else {
         return [];
